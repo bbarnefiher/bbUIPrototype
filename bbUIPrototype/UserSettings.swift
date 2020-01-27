5719886b46b37cc514b653b5488d50828115b9d0
Brian Barnefiher <bb222@pvsd.org>
2020-01-27 10:40:04 -0500

    survey stuffs

---
+++ bbUIPrototype/UserSettings.swift
@@ -0,0 +1,24 @@
+//
+//  UserSettings.swift
+//  bbUIPrototype
+//
+//  Created by pad22 on 12/13/19.
+//  Copyright © 2019 Brian J Barnefiher. All rights reserved.
+//
+
+import Foundation
+
+class UserSettings: ObservableObject {
+    @Published var textFieldAnswer1 = ""
+    @Published var textFieldAnswer2 = ""
+    @Published var textFieldAnswer3 = ""
+    @Published var textFieldAnswer4 = ""
+    @Published var textFieldAnswer5 = ""
+    @Published var textFieldAnswer6 = ""
+    @Published var textFieldAnswer7 = ""
+    @Published var textFieldAnswer8 = ""
+    @Published var pickerAnswer1 = 0
+    @Published var pickerAnswer2 = 0
+    @Published var pickerAnswer3 = 0
+    
+}
